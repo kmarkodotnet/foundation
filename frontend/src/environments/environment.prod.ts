e5d2dd3a729eb53424a12c96bdce0254f20188dc
marko <kmarko.net@gmail.com>
2026-06-02 21:57:19 +0200

    FE fix

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,4 +2,8 @@ export const environment = {
   production: true,
   apiUrl: '/api/v1',
   signalrHubUrl: '/hubs/notifications',
+  google: {
+    clientId: '130497332598-nnndjhb0eomeluh2ajlf5fbb7l1u75ck.apps.googleusercontent.com',
+    redirectUri: 'http://192.168.1.138.nip.io:8080/auth/callback',
+  },
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,4 +2,8 @@ export const environment = {
   production: false,
   apiUrl: '/api/v1',
   signalrHubUrl: '/hubs/notifications',
+  google: {
+    clientId: '130497332598-nnndjhb0eomeluh2ajlf5fbb7l1u75ck.apps.googleusercontent.com',
+    redirectUri: 'http://localhost:4200/auth/callback',
+  },
 };
